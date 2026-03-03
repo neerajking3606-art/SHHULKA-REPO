cac9608ee5b1451dbef8add1912582b4ba4247ae
gamerzh910-gif <gamerzh910@gmail.com>
2026-03-03 05:24:18 -0800

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,19 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "28389286"))
+API_HASH = environ.get("API_HASH", "b88da5f4f338cca30f8ea5fb53cb083b")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8770368784:AAGYf_18CK73cRZSsmM32zTKnCRj3s2Nkt8")
+
+OWNER = int(environ.get("OWNER", "7549194607"))
+CREDIT = environ.get("CREDIT", "🤍🌸@leavingproperty🤍🌸")
+
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7549194607').split(',')
+TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
+
+AUTH_USER = os.environ.get('AUTH_USERS', '7549194607').split(',')
+AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
+if int(OWNER) not in AUTH_USERS:
+    AUTH_USERS.append(int(OWNER))
